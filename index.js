5444538b5c1647e54b092cc838ef432e1cdcbdec
labhsingharya <77661543+labhsingharya@users.noreply.github.com>
2026-02-12 06:40:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const EXCEPT_CHATS = [
   -10011864904417
 ];
 
-const KEYWORDS = ["loot", "fast", "grab", "steal"];
+const KEYWORDS = ["loot", "fast", "grab", "steal", "buy max"];
 const REPLACE_LINK = "https://t.me/Lootdealtricky";
 const CACHE_TIME = 30 * 60 * 1000;
 
